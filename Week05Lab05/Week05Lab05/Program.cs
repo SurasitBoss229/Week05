929aebd2b7b5a8a4851d5c8fdde07591f97092f1
Surasit <surasitboss229@gmail.com>
2017-09-08 16:12:31 +0700

    W05P16

--- Week05Lab05/Week05Lab05/Program.cs
+++ Week05Lab05/Week05Lab05/Program.cs
@@ -1,28 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Week05Lab05
+using System.IO;
+public class switchLearning
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Input your grade (A, B, C, D or F) : ");
+        string gradeString = Console.ReadLine();
+        string message;
+        switch (gradeString.ToUpper())
         {
-            Console.WriteLine("Line 1");
-            Console.WriteLine("Line 2");
-            Console.WriteLine("Line 3");
-            line4:
-            Console.WriteLine("Line 4");
-            Console.WriteLine("Line 5");
-            Console.WriteLine("Line 6");
-            goto line10;
-            Console.WriteLine("Line 7");
-            Console.WriteLine("Line 8");
-            Console.WriteLine("Line 9");
-            line10:
-            Console.WriteLine("Line 10");
+            case "A":
+                message = "Excellent";
+                break;
+            case "B":
+                message = "Good";
+                break;
+            case "C":
+                message = "Cool";
+                break;
+            case "D":
+                message = "Try";
+                break;
+            case "F":
+                message = "Get out!!";
+                break;
+            default:
+                message = "Incorrect grade";
+                break;
         }
+        Console.WriteLine(message);
     }
 }
