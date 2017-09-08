8f569671888ed4746a5886f619f82edc313eec68
Surasit <surasitboss229@gmail.com>
2017-09-08 13:49:58 +0700

    W05P11

--- Week05Lab05/Week05Lab05/Program.cs
+++ Week05Lab05/Week05Lab05/Program.cs
@@ -1,28 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Week05Lab05
+using System.IO;
+public class ExceptionLearning
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        int a = 10;
+        int b = 20;
+        int c;
+        try
         {
-            Console.WriteLine("Line 1");
-            Console.WriteLine("Line 2");
-            Console.WriteLine("Line 3");
-            line4:
-            Console.WriteLine("Line 4");
-            Console.WriteLine("Line 5");
-            Console.WriteLine("Line 6");
-            goto line10;
-            Console.WriteLine("Line 7");
-            Console.WriteLine("Line 8");
-            Console.WriteLine("Line 9");
-            line10:
-            Console.WriteLine("Line 10");
+            c = div(a, b);
         }
+        catch (DivideByZeroException e)
+        {
+
+            Console.WriteLine("DivideByZeroException");
+            Console.WriteLine(e.Message);
+        }
+        catch (NullReferenceException e)
+        {
+            Console.WriteLine("NullReferenceException");
+            Console.WriteLine(e.Message);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception");
+            Console.WriteLine(e.Message);
+        }
+    }
+    private static int div(int a, int b)
+    {
+        throw new DivideByZeroException();
     }
 }
